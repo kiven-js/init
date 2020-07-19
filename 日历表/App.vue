124f2a899b98e48efa4b1625b928798e80d8b90b
root <18892410347@139.com>
2020-07-19 22:11:36 +0800

    日历表

---
+++ 日历表/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <DatePicker/>
+</template>
+
+<script>
+import DatePicker from './DatePicker';
+export default {
+    components:{
+        DatePicker,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ 日历表/DatePicker.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="date-picker">
+        <div class="picker-input">
+            <span class="input-prefix">
+                <i>xx</i>
+            </span>
+            <input type="text">
+        </div>
+        <div class="picker-panel">
+            <div class="picker-arrow"></div>
+            <div class="picker-body">
+                <div class="picker-header">
+                    <span class="picker-btn icon-prev-year"></span>
+                    <span class="picker-btn icon-prev-month" ></span>
+                    <span class="picker-date">
+                        2020年
+                    </span>
+                    <span class="picker-btn icon-next-month"></span>
+                    <span class="picker-btn icon-next-year"></span>
+                </div>
+                <div class="picker-content">
+                    <div class="picker-weeks">
+                        <div
+                         v-for="week in ['日','一','二','三','四','五','六']"
+                        :key="week"
+                        >
+                        {{ week }}
+                        </div>
+                    </div>
+                    <div class="picker-days">
+                        <div
+                            v-for="date in 42"
+                            :key="date"
+                        >
+                        {{ date }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+
+
+
+<style scoped>
+
+</style>
